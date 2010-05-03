7db5c8277c0c383cc37c79e5437262618742b447
Taher Haveliwala <oss@taherh.org>
2010-05-02 20:36:06 -0700

    fixed download_url in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
       name = "pysimsearch",
       packages = ["pysimsearch", "pysimsearch.test"],
-      version = "0.15",
+      version = "0.16",
       description = "Similarity-search library",
       author = "Taher Haveliwala",
       author_email = "oss@taherh.org",
       url = "http://code.google.com/p/pysimsearch/",
-      download_url = "http://pysimsearch.googlecode.com/files/pysimsearch-0.1.tar.gz",
+      download_url = "http://pysimsearch.googlecode.com/files/pysimsearch-0.16.tar.gz",
       keywords = ["similarity"],
       requires = ["httplib2", "libxml"],
       classifiers = [

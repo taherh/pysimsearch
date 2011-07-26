1d3e637863bb6f2cd0e7a6f4220075b820091d80
Taher Haveliwala <oss@taherh.org>
2011-07-26 01:18:59 -0700

    o updating for v0.20 release

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from distutils.core import setup
 setup(
       name = "pysimsearch",
       packages = ["pysimsearch", "pysimsearch.test"],
-      version = "0.18",
+      version = "0.20",
       description = "Similarity-search library",
       author = "Taher Haveliwala",
       author_email = "oss@taherh.org",
       url = "https://github.com/taherh/pysimsearch",
-      download_url = "",
+      download_url = "https://github.com/downloads/taherh/pysimsearch/pysimsearch-0.20.tar.gz",
       keywords = ["similarity"],
       requires = ["httplib2", "libxml"],
+      license = "BSD License",
       classifiers = [
                      "Programming Language :: Python",
                      "License :: OSI Approved :: BSD License",
@@ -20,6 +21,7 @@ Similarity-Search Library
 -------------------------
 
 Requires Python v2.7.1 or higher
-Library for measuring textual similarity of files and web pages.
+Library for measuring textual similarity of files and web pages and
+building similarity indexes.  Primarily for pedagogical purposes.
 '''
       )

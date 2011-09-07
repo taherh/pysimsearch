47580b191f8e5b479ac0fc9b29887a9409ac8062
Taher Haveliwala <oss@taherh.org>
2011-09-06 18:53:46 -0700

    o refactored sim_index.py module into sim_index/ package

---
+++ pysimsearch/sim_index/__init__.py
@@ -0,0 +1,4 @@
+from .sim_index import SimIndex
+from .simple_memory_sim_index import SimpleMemorySimIndex
+from .sim_index_collection import SimIndexCollection
+from .remote_sim_index import RemoteSimIndex
